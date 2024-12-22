9b164d3716c0d0b774bae8ca19135f0b8607d24f
Tuturu <edu.sanchez@unmsm.edu.pe>
2024-12-21 23:28:13 -0500

    actulaizacion

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const createTaskRequest= async (task) => {
-    return await axios.post(`http://localhost:4000/tasks`, task);
+    return await axios.post(`http://localhost:3000/tasks`, task);
 }
 
 export const getTasksRequest = async () => {
-    return await axios.get(`http://localhost:4000/tasks`);
+    return await axios.get(`http://localhost:3000/tasks`);
 } 
 
 export const deleteTaskRequest = async (id) => {
-    return await axios.delete(`http://localhost:4000/tasks/${id}`);
+    return await axios.delete(`http://localhost:3000/tasks/${id}`);
 }
--- server/index.js
+++ server/index.js
@@ -2,31 +2,22 @@ import express from "express";
 import { PORT } from "./config.js";
 import indexRoutes from "./routes/index.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
+import {fileURLToPath} from "url";
+import {dirname,join} from "path";
 import cors from "cors";
 
 const app = express();
-
-// Resuelve la ubicación del directorio actual
-const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(`Current directory: ${__dirname}`);
-
-// Middlewares
-app.use(cors());
+const __dirname=dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
+app.use(cors(
+    {
+        origin:"http://localhost:4000"
+    }
+));
 app.use(express.json());
-
-// Rutas
 app.use(indexRoutes);
 app.use(taskRoutes);
+app.use(express.static(join(__dirname,'../client/dist')));
 
-// Archivos estáticos
-app.use(express.static(join(__dirname, "../client/dist")));
-
-// Puerto dinámico (Render usa el puerto definido en PORT o asigna uno automáticamente)
-const port = PORT || 3000;
-
-// Inicia el servidor
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
